96f8a6c978974022c16c1991a53bb11f6dae0d23
Shako Lomtadze <shakolomtadze00@gmail.com>
2024-01-10 10:44:03 +0400

    first commit

---
+++ app/src/main/java/com/example/challenge/App.kt
@@ -0,0 +1,6 @@
+package com.example.challenge
+
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : android.app.Application()
---
+++ app/src/main/java/com/example/challenge/presentation/screen/splash/SplashFragment.kt
@@ -0,0 +1,40 @@
+package com.example.challenge.presentation.screen.splash
+
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.fragment.findNavController
+import com.example.challenge.databinding.FragmentSplashBinding
+import com.example.challenge.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.channels.BufferOverflow
+import kotlinx.coroutines.flow.buffer
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+    private val viewModel: SplashViewModel by viewModels()
+
+    override fun bindObserves() {
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiEvent.buffer(1, BufferOverflow.DROP_LATEST).collect {
+                    handleNavigationEvents(event = it)
+                }
+            }
+        }
+    }
+
+    private fun handleNavigationEvents(event: SplashViewModel.SplashUiEvent) {
+        when (event) {
+            is SplashViewModel.SplashUiEvent.NavigateToConnections -> findNavController().navigate(
+                SplashFragmentDirections.actionSplashFragmentToFriendsFragment()
+            )
+
+            is SplashViewModel.SplashUiEvent.NavigateToLogIn -> findNavController().navigate(
+                SplashFragmentDirections.actionSplashFragmentToLogInFragment()
+            )
+        }
+    }
+}
